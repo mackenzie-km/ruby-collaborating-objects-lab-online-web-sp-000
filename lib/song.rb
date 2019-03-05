1ed67a721d6933abdb85cab5f0a4dd5598475903
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-05 21:37:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,7 @@ class Song
     found_artist = Artist.find_or_create_by_name(filename[0])
     filename[1] = Song.new(filename[1])
     filename[1].artist = found_artist
+    found_artist.songs << filename[1]
     binding.pry
     filename[1]
   end 
