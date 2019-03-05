f1a74c747775a907e7a7d7cd6f942828a47ac193
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-05 20:50:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,12 @@ class Song
     @name = name 
   end 
 
+require "pry"
   def self.new_by_filename(input)
     filename = input.split(" - ")
-    filename[1] = self.new("#{filename[1]}")
+    filename[1] = self.new(filename[1])
     filename[1].artist = filename[0]
+    binding.pry
   end 
   
 end 
