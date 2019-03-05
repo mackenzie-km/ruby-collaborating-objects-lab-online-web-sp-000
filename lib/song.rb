03c4eda75bbdf32d0123775cfda90d28cc4577ca
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-05 20:44:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,7 @@ class Song
   def self.new_by_filename(filename)
     filename = filename.split(" - ")
     filename[1] = self.new(filename[1])
+    filename[1].artist = filename[0]
   end 
   
 end 
