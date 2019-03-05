a5a7cebf8491f5f3815af67df17f689833eea9d3
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-05 21:35:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist
+  require "pry"
   def initialize(name)
     @name = name 
   end 
@@ -9,6 +10,7 @@ class Song
     found_artist = Artist.find_or_create_by_name(filename[0])
     filename[1] = Song.new(filename[1])
     filename[1].artist = found_artist
+    binding.pry
     filename[1]
   end 
   
