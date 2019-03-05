913315ec3e60668d14a08177ec9c2795c547da8b
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-05 20:51:43 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,7 +2,7 @@
  class MP3Importer
 
   attr_accessor :path, :files
-  
+  require "pry"
   def initialize(path)
     @path = path
   end 
@@ -23,6 +23,7 @@ def import
   files 
 @files.each do |file|
   Song.new_by_filename(file)
+  binding.pry
 end 
 end
   
