75507e34339a784f6233f6712b8a08cbd15758fd
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-05 20:49:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   end 
 
   def self.new_by_filename(input)
-    filename = filename.split(" - ")
+    filename = input.split(" - ")
     filename[1] = self.new(filename[1])
     filename[1].artist = filename[0]
   end 
